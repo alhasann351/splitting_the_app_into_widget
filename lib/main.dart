d04ca3b2f60b00b500df38caa7128ec735c1a342
alhasann351 <alhasann352@gmail.com>
2024-02-26 21:08:51 +0600

    splitting app into widget

--- lib/main.dart
+++ lib/main.dart
@@ -42,89 +42,117 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           //mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.blue,
-                child: ListView.builder(
-                  itemBuilder: (context, index) => const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      width: 100,
-                      child: CircleAvatar(
-                        backgroundColor: Colors.red,
-                      ),
-                    ),
-                  ),
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 4,
-              child: Container(
-                color: Colors.green,
-                child: ListView.builder(
-                  itemBuilder: (context, index) => const ListTile(
-                    title: Text(
-                      'Hasan',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 22,
-                      ),
-                    ),
-                    subtitle: Text('Flutter Developer'),
-                    leading: CircleAvatar(child: Icon(Icons.person_outline)),
-                    trailing: Icon(Icons.share_outlined),
-                  ),
-                  itemCount: 10,
-                  //scrollDirection: Axis.horizontal,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                color: Colors.red,
-                child: ListView.builder(
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      color: Colors.blue,
-                      height: 60,
-                      width: 60,
-                    ),
-                  ),
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 2,
-              child: Container(
-                color: Colors.purple,
-                child: GridView.builder(
-                  gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-                    maxCrossAxisExtent: 200,
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                  ),
-                  itemBuilder: (context, index) => Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Container(
-                      color: Colors.orange,
-                      width: 100,
-                      height: 100,
-                    ),
-                  ),
-                  itemCount: 10,
-                  scrollDirection: Axis.horizontal,
-                ),
-              ),
-            ),
+            CategoryIcon(),
+            PersonInfo(),
+            Category(),
+            CategoryDetails(),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryIcon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.blue,
+        child: ListView.builder(
+          itemBuilder: (context, index) => const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: SizedBox(
+              width: 100,
+              child: CircleAvatar(
+                backgroundColor: Colors.red,
+              ),
+            ),
+          ),
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class PersonInfo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 4,
+      child: Container(
+        color: Colors.green,
+        child: ListView.builder(
+          itemBuilder: (context, index) => const ListTile(
+            title: Text(
+              'Hasan',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 22,
+              ),
+            ),
+            subtitle: Text('Flutter Developer'),
+            leading: CircleAvatar(child: Icon(Icons.person_outline)),
+            trailing: Icon(Icons.share_outlined),
+          ),
+          itemCount: 10,
+          //scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class Category extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.red,
+        child: ListView.builder(
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.blue,
+              height: 60,
+              width: 60,
+            ),
+          ),
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 2,
+      child: Container(
+        color: Colors.purple,
+        child: GridView.builder(
+          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 200,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              color: Colors.orange,
+              width: 100,
+              height: 100,
+            ),
+          ),
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
         ),
       ),
     );
